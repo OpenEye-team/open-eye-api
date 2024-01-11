f5da98bc98e7eb3b4ec2b30bab36909af150d5f5
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2024-01-11 13:18:17 +0700

    setup project

---
+++ src/service/index.ts
@@ -0,0 +1,3 @@
+export * from './auth.service'
+export * from './glucometer.service'
+export * from './article.service'
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export * from './jwt'
+export * from './auth'
+export * from './glucometer'
